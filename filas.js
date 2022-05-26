659c15209e06d5712f3f5da8bdc570c0aa4a5058
victorcova <victor.avoc@gmail.com>
2022-05-26 11:12:18 -0300

    Update filas.js

--- filas.js
+++ filas.js
@@ -1,32 +1,6 @@
-console.log('----- TREINAMENTO TERA SETS: -----');
+console.log('----- TREINAMENTO TERA FILAS: -----');
 
-// Criando um conjunto vazio
-// const emptySet = new Set(); 
+// CONCEITO FIFO: FIRT IN FIRST OUT -> O PRIMEIRO A ENTRAR É O PRIMEIRO A SAIR.
 
-// Criando um conjunto a partir de um array
-const set = new Set([45,68,92]);
-
-// Verificando se existe elemento
-console.log('No SET há a chave 68?\n',set.has(68));
-// Verificando se existe elemento
-console.log('No SET há a chave 1?\n',set.has(1)); // false
-
-// Percorrendo um conjunto
-console.log('Percorrendo um SET com o loop FOR/OF:\n',)
-for(const item of set) {    
-    console.log('CHAVE: ',item);
-}
-
-// Inserindo um elemento
-set.add(4);
-console.log('Adicionando ao SET a chave 4:\n',set);
-
-// Removendo um elemento
-set.delete(4);
-console.log('Removendo do SET a chave 4:\n',set);
-
-// Removendo todos os elementos
-set.clear();
-console.log('Removendo todos os valores do SET:\n',set);
 
 console.log('---------------');
