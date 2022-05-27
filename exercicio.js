1be97efa7a82e17bf36a508291f55222cb881055
victorcova <victor.avoc@gmail.com>
2022-05-27 12:30:32 -0300

    Update exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -1,18 +1,32 @@
-// Entrada: 
-array = [8, 7, 2, 5, 3, 1]
-soma = 10
- 
-// Saída: 
-// Par encontrado (8, 2)
-// ou
-// Par encontrado (7, 3)
+console.log('----- EXERCÍCIOS ARRAY: -----');
 
+// Entrada 1: 
+// array = [8, 7, 2, 5, 3, 1];
+// soma = 10;
 
+// Entrada 2: 
+array = [5, 2, 6, 8, 1, 9];
+soma = 12;
+response = 0;
 
+for (let a = 0; a < array.length; a++) {
+    let currentNum = array[a];
 
-// Entrada: 
-// array = [5, 2, 6, 8, 1, 9]
-// soma = 12
- 
-// Saída: 
-// Nenhum par encontrado
+    for (let b = 0; b < array.length; b++) {
+        let numTeste = array[b];
+        let somatorio = currentNum + numTeste;        
+
+        if (array[a] !== array[b]) {
+            if (somatorio == soma) {
+                response = `Par encontrado (igual a ${soma}): (${array[a]},${array[b]})\n`;
+                console.log(response);
+            }
+        }        
+    }        
+}
+
+if (response === 0) {
+    console.log(`Nenhuma soma dos elementos do array foi igual a ${soma}.`)
+}
+
+console.log('---------------');
