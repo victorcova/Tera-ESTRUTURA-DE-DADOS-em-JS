b2e9cd2c38eb80748626bad9b3e17f9df87d4ca7
victorcova <victor.avoc@gmail.com>
2022-05-26 21:36:46 -0300

    ok

---
+++ exercicio.js
@@ -0,0 +1,18 @@
+// Entrada: 
+array = [8, 7, 2, 5, 3, 1]
+soma = 10
+ 
+// Saída: 
+// Par encontrado (8, 2)
+// ou
+// Par encontrado (7, 3)
+
+
+
+
+// Entrada: 
+// array = [5, 2, 6, 8, 1, 9]
+// soma = 12
+ 
+// Saída: 
+// Nenhum par encontrado
